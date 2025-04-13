2b67f65a7e7433516ab26d5f6b33886362d544c7
mokasp <kasperbouldin@gmail.com>
2025-04-13 00:42:04 -0500

    (add) route for prediction

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ from flask import Flask, request, render_template
 from datetime import datetime as dt
 import tensorflow as tf
 import logging
-from display_colors import display_colors
+from prepare_input import display_colors, model_input, display_prediction
 
 
 app = Flask(__name__)
@@ -63,6 +63,39 @@ def palette():
                 output64 = base64.b64encode(buffer).decode('utf-8')
 
                 return f'<h2>Image received!</h2><img src="data:image/jpeg;base64,{img_base64}" width="300"><img src="data:image/jpeg;base64,{img_array64}" width="300"><img src="data:image/jpeg;base64,{output64}" width="300">'
+            else:
+                return 'Error decoding image'
+        else:
+            return 'Invalid image data'
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    model = tf.keras.models.load_model('/model/test_model_00.keras')
+    if request.method == 'POST':
+        data_url = request.form['image']  # base64 string
+
+        if data_url.startswith('data:image'):
+            header, encoded = data_url.split(",", 1)
+            image_data = base64.b64decode(encoded)
+            np_arr = np.frombuffer(image_data, np.uint8)
+            img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            if img is not None:
+                _, buffer = cv2.imencode('.jpg', img)
+                img_base64 = base64.b64encode(buffer).decode('utf-8')
+                img_type = str(type(img_base64))
+                logging.debug("🎯 /palette route was hit!")
+                logging.debug(f"📸 Image data starts with: {request.form['image'][:30]}")
+
+                prediction = model_input(img, model)
+                logging.debug(f"🧠 prediction: {prediction}")
+                output = display_prediction(prediction)
+                logging.debug(f"🧠 output: {output}")
+
+
+                return f'<h2>Image received!</h2><img src="data:image/jpeg;base64,{img_base64}" width="300"><img src="data:image/jpeg;base64,{output}" width="300">'
             else:
                 return 'Error decoding image'
         else:
