f8f267cc244c4396b26ad83345712537e766afe4
mokasp <kasperbouldin@gmail.com>
2025-04-13 00:42:19 -0500

    (add) display prediction

---
+++ prepare_input.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+import cv2
+from prediction_input import load_data, normalize_vectors
+from lab_to_rgb import lab_to_rgb
+from make_grid import make_grid
+from output_image import output_image
+
+def display_colors(image):
+
+    X, y = load_data(image)
+
+    X_rgb = lab_to_rgb(X)
+    y_rgb = lab_to_rgb(y)
+
+    img_array = make_grid(X_rgb)
+    output = output_image(y_rgb)
+
+    return img_array, output
+
+def model_input(image, model):
+    X, _ = load_data(image)
+
+    prediction = model.predict(X)
+
+    return prediction
+
+def display_prediction(y_lab):
+    y_rgb = lab_to_rgb(y_lab)
+    output = output_image(y_rgb)
+    return output
+
