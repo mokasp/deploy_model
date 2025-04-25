227b9cf243f8681bbacb6e0e01cec0fc99ed9006
mokasp <kasperbouldin@gmail.com>
2025-04-25 17:49:03 -0500

    (add) importing model in subprocess

--- flask_app.py
+++ flask_app.py
@@ -4,12 +4,8 @@ import cv2
 import numpy as np
 from flask import Flask, request, render_template
 from datetime import datetime as dt
-import tensorflow as tf
 import logging
-from prepare_input import display_colors, model_input, display_prediction
-import gc
-import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
+from prepare_input import display_colors, model_input, display_prediction, predict_in_subprocess
 
 
 app = Flask(__name__)
@@ -40,7 +36,6 @@ def index():
 @app.route('/palette', methods=['GET', 'POST'])
 def palette():
     if request.method == 'POST':
-        model = tf.keras.models.load_model('model/test_model_00.keras', compile=False)
         data_url = request.form['image']  # base64 string
 
         if data_url.startswith('data:image'):
@@ -86,17 +81,13 @@ def predict():
             img = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
 
             if img is not None:
-                model = tf.keras.models.load_model('model/test_model_00.keras', compile=False)
                 _, buffer = cv2.imencode('.jpg', img)
                 img_base64 = base64.b64encode(buffer).decode('utf-8')
                 img_type = str(type(img_base64))
                 logging.debug("🎯 /palette route was hit!")
                 logging.debug(f"📸 Image data starts with: {request.form['image'][:30]}")
 
-                prediction = model_input(img, model)
-                del model
-                tf.keras.backend.clear_session()
-                gc.collect()
+                prediction = predict_in_subprocess(img)
                 logging.debug(f"🧠 prediction: {prediction}")
                 output = display_prediction(prediction)
                 _, buffer = cv2.imencode('.jpg', output)
--- prepare_input.py
+++ prepare_input.py
@@ -5,6 +5,8 @@ from lab_to_rgb import lab_to_rgb
 from make_grid import make_grid
 from output_image import output_image
 import logging
+from multiprocessing import Process, Queue
+import gc
 
 
 def display_colors(image):
@@ -31,3 +33,15 @@ def display_prediction(y_lab):
     output = output_image(y_rgb)
     return output
 
+def model_runner(img_data, q):
+    import tensorflow as tf
+    model = tf.keras.models.load_model('model/test_model_00.keras', compile=False)
+    prediction = model_input(img_data, model)
+    q.put(prediction)
+
+def predict_in_subprocess(img_data):
+    q = Queue()
+    p = Process(target=model_runner, args=(img_data, q))
+    p.start()
+    p.join()
+    return q.get()
